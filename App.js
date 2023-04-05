fe644fa6d7594ceafa73b1ad4deea401971e2288
saicharanmygit <125890282+saicharanmygit@users.noreply.github.com>
2023-04-05 23:25:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+//import ContactUs from "./Components/ContactUs";
+import Navigation from "./Components/Navigation";
+import Isin from "./Components/Isin";
+import Contactme from "./Components/Contactme";
+
+function App(){
+  return(
+    <div className="App">
+      <Navigation/>
+      {/* <ContactUs/> */}
+      {/* <Isin/> */}
+      {/* <Contactme/> */}
+    </div>)
+}
+export default App;
